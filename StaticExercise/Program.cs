3f793e8bfd3cc16535d2c28d26ec6106603b8340
Ebo Onwunali <ebo@njgeneral.com>
2022-01-24 16:33:46 -0500

    First commit - finished code for Static Exercise

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"What is the temperature in fahrenheit you would like to convert\n");
+            var userInput = double.TryParse(Console.ReadLine(), out double result);
+            Console.WriteLine($"\nThis converts to {TempConverter.FahrenheitToCelcius(result)} degrees celcius...\n");
+
+            Console.WriteLine($"----------------------------------");
+
+            Console.WriteLine($"\nWhat is the temperature in celcius you would like to convert\n");
+            var userInputC = double.TryParse(Console.ReadLine(), out double result2);
+            Console.WriteLine($"\nThis converts to {TempConverter.CelciusToFahrenheit(result2)} degrees celcius...\n");
+
+            Console.WriteLine($"----------------------------------");
+
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+
+
+        public static double FahrenheitToCelcius (double toCelcius)
+        {
+            var celcius = (toCelcius - 32) * 5 / 9;
+            return celcius;
+        }
+
+        public static double CelciusToFahrenheit(double toFahren)
+        {
+            var fahren = (toFahren * 9) / 5 + 32;
+            return fahren;
+        }
+
+    }
+}
